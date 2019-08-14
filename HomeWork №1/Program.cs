e63882818cf154d378c414863f2c2d75425a708e
Vladislav Kuznetsov <vlad.kuznetsov.97@gmail.com>
2019-08-15 01:36:21 +0300

    Add project files.

---
+++ HomeWork №1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork__1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Vladislav Kuznetsov";
+            string group = "C-19-03";
+            Console.WriteLine("Hello!!!");
+            Console.WriteLine("Name : {0}", name);
+            Console.WriteLine($"Group : {group}");
+            Console.ReadLine();
+        }
+    }
+}
